2edac4533a50ee26453cf9401f29d3bc6979349c
onderfrt <96356562+onderfrt@users.noreply.github.com>
2023-07-22 00:29:17 +0300

    Add files via upload

---
+++ ConsumerApp/ConsumerApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace ConsumerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bağlantı Oluşturma
+
+            ConnectionFactory factory = new();
+            factory.Uri = new("amqps://alchltry:s4vsK9XkMABWTy_UOQuEOaFn3zgtR1xa@sparrow.rmq.cloudamqp.com/alchltry");
+
+            //Bağlantı Aktifleştirme ve Kanal Açma
+            using IConnection connection = factory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+
+            //Queue Oluşturma
+            channel.QueueDeclare(queue: "example-queue", exclusive: false);  //Consumer'da da kuyruk publisher'daki ile birebir aynı yapılandırmada tanımlanmalıdır!
+
+            //Queue'dan Mesaj Okuma
+            EventingBasicConsumer consumer = new(channel);
+
+            channel.BasicConsume(queue: "example-queue", false, consumer);
+
+            consumer.Received += (sender, e) =>
+            {
+                //Kuyruğa gelen mesajın işlendiği yerdir!
+                //e.Body: Kuyruktaki mesajın verisini bütünsel olarak getirecektir.
+                //e.Body.Span veya e.Body.ToArray()  : Kuyruktaki mesajın byte veirisini getirecektir.
+
+                Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+            };
+
+            Console.Read();
+        }
+    }
+}
---
+++ PublisherApp/PublisherApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublisherApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Bağlantı oluşturma
+            ConnectionFactory factory = new();
+
+            factory.Uri = new("amqps://alchltry:s4vsK9XkMABWTy_UOQuEOaFn3zgtR1xa@sparrow.rmq.cloudamqp.com/alchltry");
+
+            //Bağlantıyı aktifleştirme ve kanal açma
+
+            using IConnection connection = factory.CreateConnection();
+
+            using IModel channel = connection.CreateModel();
+
+            //Quee Oluşturma
+            channel.QueueDeclare(queue: "example-queue", exclusive:false);
+
+            //Queue'ye Mesaj Gönderme
+
+            //RabbirMQ kuyruğa atacağı mesajları byte türünden kabul etmektedir.
+
+            //byte[] message = Encoding.UTF8.GetBytes("Merhaba");
+
+            //channel.BasicPublish(exchange: "",routingKey:"example-queue",body:message);
+
+            for (int i = 0; i < 100; i++)
+            {
+                Task.Delay(200);
+                byte[] message = Encoding.UTF8.GetBytes("RabbitMQ Test" + " " + i);
+
+                channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message);
+            }
+
+            Console.Read();
+
+        }
+
+        
+    }
+}
